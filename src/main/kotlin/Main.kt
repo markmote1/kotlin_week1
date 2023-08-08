bbb3077f1580a389eba682f623e56b9d0d6e0b70
symptoms-vader <symptomsvader@gmail.com>
2023-08-08 09:31:28 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //variables in kotlin
+    var name = "Kim"
+    var age = 78
+    println(name)
+    println(age)
+
+
+
+    var city:String = "Nairobi"
+    var idnum:Int = 47
+
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var num1 = 20
+    var num2 = 15
+
+    var result = num1+num2
+    println(result)
+
+    var no1 = 10
+    var no2 = 12
+
+    var ans = no1 * no2
+    println(ans)
+
+    var n1 = 50
+    var n2 = 10
+
+    var ok = n1/n2
+    println(ok)
+}
